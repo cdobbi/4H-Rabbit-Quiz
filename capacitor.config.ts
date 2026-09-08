6af94827471cff3f92ff2ebb4a384ec050c1112e
Christina Dobbins <u800176908@byui.edu>
2026-09-08 00:57:49 -0600

    ...

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.cdobbi.rabbithusbandryquiz',
+  appName: 'Rabbit Husbandry Quiz',
+  webDir: 'www'
+};
+
+export default config;
